1c0cf0271be7f3bf83793231e1a523b5296252f1
Аршинов Алексей <arshinov_an@stack.stack-it.ru>
2023-03-29 17:35:23 +0300

    feat: add local cache chart data

--- src/components/StockChart.vue
+++ src/components/StockChart.vue
@@ -5,48 +5,75 @@
     :redrawOnUpdate="true"
     :oneToOneUpdate="true"
     :animateOnUpdate="true"
+    ref="chart"
   />
 </template>
 
 <script setup lang="ts">
-import { reactive, watchEffect } from "vue";
+import { ref, reactive, watchEffect } from "vue";
 import VueHighcharts from "vue3-highcharts";
 import HighCharts from "highcharts";
 import StockCharts from "highcharts/modules/stock";
 
 import { getHistoryStockQuotes } from "../api/moex";
 
-const props = defineProps<{ securitie?: Securitie, fromDate: string, toDate: string }>();
+const props = defineProps<{
+  securitie?: Securitie;
+  fromDate: string;
+  toDate: string;
+}>();
 
 StockCharts(HighCharts);
 
 type ChartData = [number, number];
+type SecuritieData = {
+  [index: string]: ChartData[];
+};
 
+const chacheData: SecuritieData = {};
+
+const chart = ref(null);
 const chartOptions = reactive({
-      rangeSelector: {
-         selected: 4,
-      },
-      series: [
-         {
-            name: props.securitie?.secid ?? '',
-            data: [] as ChartData[],
-         },
-      ],
-   }
-);
+  rangeSelector: {
+    selected: 1,
+  },
+  series: [
+    {
+      name: "",
+      data: [] as ChartData[],
+    },
+  ],
+});
 
 watchEffect(async () => {
-   if (props.securitie) {
-      const data = await getHistoryStockQuotes(props.securitie.secid, props.securitie.group, props.securitie.boardid, {
-       fromDate: props.fromDate,
-       toDate: props.toDate,
-      });
-      chartOptions.series[0].name = props.securitie?.secid ?? '';
-      chartOptions.series[0].data.splice(0);
-      chartOptions.series[0].data.push(...data.map((item: SecuritiePrice) => [Date.parse(item.date), item.volume] as ChartData));
-   } else {
-      chartOptions.series[0] = { name: '', data: [] };
-   }
-})
+  let chartData: ChartData[] = [];
+  if (props.securitie) {
+    if (chacheData[props.securitie.secid]) {
+      chartData = chacheData[props.securitie.secid];
+    } else {
+      // @ts-ignore
+      chart.value.chart.showLoading();
+      const data = await getHistoryStockQuotes(
+        props.securitie.secid,
+        props.securitie.group,
+        props.securitie.boardid,
+        {
+          fromDate: props.fromDate,
+          toDate: props.toDate,
+        }
+      );
 
+      chartData = data.map(
+        (item: SecuritiePrice) =>
+          [Date.parse(item.date), item.volume] as ChartData
+      );
+      // @ts-ignore
+      chart.value.chart.hideLoading();
+
+      chacheData[props.securitie.secid] = chartData;
+    }
+  }
+  chartOptions.series[0].name = props.securitie?.secid ?? "";
+  chartOptions.series[0].data = chartData;
+});
 </script>
